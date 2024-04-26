31891f7ead3413998aa6ab2560a6dfb09e6d6fdc
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-04-26 22:24:12 +0600

    react-simple-typewriter added

--- src/Pages/ArtSection.jsx
+++ src/Pages/ArtSection.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { url } from "../utility/url";
 import ArtCart from "./ArtCart";
+import { Typewriter } from "react-simple-typewriter";
 
 
 const ArtSection = () => {
@@ -12,7 +13,7 @@ const ArtSection = () => {
     }, [])
     return (
         <div>
-            <h2 className="text-2xl my-10 text-center underline md:text-5xl text-[#FF497C] font-bold">Explore Our Product</h2>
+            <h2 className="text-2xl my-10 text-center underline md:text-5xl text-[#FF497C] font-bold">Explore <Typewriter words={['Our Product']} delaySpeed={1000} typeSpeed={100} loop={0} /></h2>
             <div className="grid  grid-cols-1 gap-4 md:gap-10 md:grid-cols-3 lg:grid-cols-4">
                 {art.slice(0, 8).map(art => <ArtCart key={art._id} art={art}></ArtCart>)}
             </div>
