6f09a7cf88101a9fe8a41e6742f350eee09f62a0
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-04-27 16:29:18 +0600

    my art and craft filtering added

--- src/Pages/MyArt.jsx
+++ src/Pages/MyArt.jsx
@@ -4,6 +4,14 @@ import { url } from "../utility/url";
 import MyArtList from "./MyArtList";
 import { Helmet } from "react-helmet-async";
 import NoItem from "./NoItem";
+import { IoIosArrowDown } from "react-icons/io";
+import {
+    Menu,
+    MenuHandler,
+    MenuList,
+    MenuItem,
+    Button,
+} from "@material-tailwind/react";
 
 const MyArt = () => {
     const { user } = useContext(AuthContext);
@@ -30,16 +38,50 @@ const MyArt = () => {
             });
     };
 
+    const [filterType, setFilterType] = useState("yes");
+
+    const handleFilterClick = (filter) => {
+        setFilterType(filter);
+    };
+
+    const sortItem = (item, filter) => {
+        if (filter === "yes") {
+            return item;
+        } else {
+            return item.slice().sort((a, b) => {
+                if (a[filter] < b[filter]) {
+                    return -1;
+                }
+                if (a[filter] > b[filter]) {
+                    return 1;
+                }
+                return 0;
+            });
+        }
+    };
+
     return (
         <div>
             <Helmet>
                 <title>My Art & Craft Item</title>
             </Helmet>
+            <div className="text-center">
+                <Menu>
+                    <MenuHandler>
+                        <Button className="bg-[#23BE0A] mt-5 w-fit md:w-44"><span className="flex items-center justify-center gap-2">Sort By <IoIosArrowDown className="text-2xl font-bold" /></span></Button>
+                    </MenuHandler>
+                    <MenuList className="p-5  w-44 border-2 space-y-3">
+                        <MenuItem className="hover:bg-green-400 rounded-lg py-2" onClick={() => handleFilterClick("yes")}>Yes</MenuItem>
+                        <MenuItem className="hover:bg-green-400 rounded-lg py-2" onClick={() => handleFilterClick("customization")}>No</MenuItem>
+                    </MenuList>
+                </Menu>
+            </div>
             <h2 className="text-4xl font-bold text-center underline text-blue-700 my-5">My Art & Craft Item</h2>
             {items.length > 0 ?
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-5">
+                    
                     {
-                        items.map(item => <MyArtList key={item._id} handleDelete={handleDelete} item={item}></MyArtList>)
+                        sortItem(items, filterType).map(item => <MyArtList key={item._id} handleDelete={handleDelete} item={item}></MyArtList>)
                     }
                 </div>
                 : <> <NoItem /> </>}
