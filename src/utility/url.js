5a33f9d7ef3cc5e70f103951c1420b456387d505
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-04-27 21:45:19 +0600

    vercel url added

--- src/utility/url.js
+++ src/utility/url.js
@@ -1,2 +1,2 @@
-// export const url = 'https://server-side-indol.vercel.app'
-export const url = 'http://localhost:5000'
+export const url = 'https://server-side-indol.vercel.app'
+// export const url = 'http://localhost:5000'
